d05fcb48f0a485858e2e15213f154a6507e44e50
Cynthia Z E MacLeod (C3D) <cynthia.macleod@cloud3d.ltd>
2025-04-26 03:32:01 +0100

    Add more granular ApplicationHostConfiguration extensions.
    Refactor ApplicationHostConfigurationExtensions
    
    Improved handling of `applicationhost.config` file by introducing a constant for the filename, adding new methods for loading and saving configurations, and restructuring existing methods. Enhanced code readability and maintainability, and improved management of temporary configuration files for better resource handling.

--- src/C3D/Extensions/Aspire/IISExpress/ApplicationHostConfiguration/ApplicationHostConfigurationExtensions.cs
+++ src/C3D/Extensions/Aspire/IISExpress/ApplicationHostConfiguration/ApplicationHostConfigurationExtensions.cs
@@ -1,4 +1,6 @@
-﻿using C3D.Extensions.Aspire.IISExpress.Configuration;
+﻿using Aspire.Hosting;
+using C3D.Extensions.Aspire.IISExpress.Annotations;
+using C3D.Extensions.Aspire.IISExpress.Configuration;
 using C3D.Extensions.Aspire.IISExpress.Resources;
 using System.Xml.Serialization;
 
@@ -6,35 +8,81 @@ namespace C3D.Extensions.Aspire.IISExpress;
 
 internal static class ApplicationHostConfigurationExtensions
 {
-    public static ApplicationHostConfiguration GetDefaultConfiguration(this IISExpressResource iis)
+    private const string applicationhostfileName = "applicationhost.config";
+
+    public static ApplicationHostConfiguration GetDefaultConfiguration(this IISExpressResource iis) =>
+        iis.Bitness.GetDefaultConfiguration();
+
+    public static ApplicationHostConfiguration GetDefaultConfiguration(this IISExpressProjectResource project) =>
+        project.Bitness.GetDefaultConfiguration();
+
+    public static ApplicationHostConfiguration GetDefaultConfiguration(this IISExpressBitness bitness)
     {
-        var configPath = Path.Combine(iis.Directory, "config", "templates", "PersonalWebServer", "applicationhost.config");
-
-        var serializer = new XmlSerializer(typeof(ApplicationHostConfiguration));
-
-        using var fs = File.OpenRead(configPath);
-        return serializer.Deserialize(fs) as ApplicationHostConfiguration ?? throw new InvalidOperationException("Could not load default applicationhost.config for IIS Express");
+        var configPath = Path.Combine(bitness.GetIISExpressPath(), "config", "templates", "PersonalWebServer", applicationhostfileName);
+        return Load(configPath) ?? throw new InvalidOperationException("Could not load default applicationhost.config for IIS Express");
     }
 
-    public static void Save(this ApplicationHostConfiguration config, Stream stream)
-    {
-        var serializer = new XmlSerializer(typeof(ApplicationHostConfiguration));
+    public static ApplicationHostConfiguration? LoadConfiguration(this ConfigArgumentAnnotation annotation) => 
+        Load(annotation.ApplicationHostConfig);
 
-        serializer.Serialize(stream, config);
+    public static void SaveConfiguration(this ConfigArgumentAnnotation annotation, ApplicationHostConfiguration config)
+    {
+        config.Save(annotation.ApplicationHostConfig);
     }
 
     public static string SaveConfiguration(this IISExpressResource iis, ApplicationHostConfiguration config)
     {
         var temp = iis.GetConfigurationPath();
-
-        using var fs = File.OpenWrite(temp);
-        config.Save(fs);
+        config.Save(temp);
 
         return temp;
     }
 
-    public static string GetConfigurationPath(this IISExpressResource iis)
+    public static ConfigArgumentAnnotation WithTemporaryConfiguration(this IISExpressProjectResource resource, ApplicationHostConfiguration config)
     {
-        return Path.Combine(iis.TempDirectory, "applicationhost.config");
+        var annotation = new ConfigArgumentAnnotation(GetTempConfigFile());
+        annotation.SaveConfiguration(config);
+        var old = resource.Annotations.OfType<ConfigArgumentAnnotation>().ToList();
+        foreach(var a in old)
+        {
+            resource.Annotations.Remove(a);
+        }
+        resource.Annotations.Add(annotation);
+        return annotation;
     }
+
+    public static string GetConfigurationPath(this IISExpressResource iis) => Path.Combine(iis.TempDirectory, applicationhostfileName);
+
+    private static ApplicationHostConfiguration? Load(string configPath)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(configPath, nameof(configPath));
+        var serializer = new XmlSerializer(typeof(ApplicationHostConfiguration));
+        if (!File.Exists(configPath))
+        {
+            return null;
+        }
+        using var fs = File.OpenRead(configPath);
+        return serializer.Deserialize(fs) as ApplicationHostConfiguration;
+    }
+
+    public static void Save(this ApplicationHostConfiguration config, Stream stream)
+    {
+        var serializer = new XmlSerializer(typeof(ApplicationHostConfiguration));
+        serializer.Serialize(stream, config);
+    }
+
+    public static void Save(this ApplicationHostConfiguration config, string fileName)
+    {
+        using var fs = File.OpenWrite(fileName);
+        config.Save(fs);
+    }
+
+    internal static string GetTempConfigDir() =>
+        System.IO.Directory.CreateTempSubdirectory("aspire.iisexpress.").FullName;
+
+    internal static string GetTempConfigFile() => Path.Combine(GetTempConfigDir(), applicationhostfileName);
+
+    public static Site? GetSite(this ApplicationHostConfiguration appHostConfig, string siteName) =>
+        appHostConfig.SystemApplicationHost.Sites.Site
+            .SingleOrDefault(s => string.Equals(s.Name, siteName, StringComparison.OrdinalIgnoreCase));
 }
