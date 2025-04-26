852c78ffe123bdf6f92ccf2c8482b32c27b482ce
Cynthia Z E MacLeod (C3D) <cynthia.macleod@cloud3d.ltd>
2025-04-26 03:55:34 +0100

    Rework IISEndPointConfigurator to allow creating and modifing the config.
    Uses previous refactoring in IISExpressExtensions.
    
    Enhance IISEndPointConfigurator with eventing support
    
    - Added `IDistributedApplicationEventing` dependency to the constructor.
    - Refactored `AddBindings` to improve application host configuration handling, including loading and creating default configurations.
    - Removed `AddBinding` method; integrated its functionality into `AddBindings`.
    - Improved endpoint management by checking for existing endpoints and adding new ones as needed.

--- src/C3D/Extensions/Aspire/IISExpress/IISEndPointConfigurator.cs
+++ src/C3D/Extensions/Aspire/IISExpress/IISEndPointConfigurator.cs
@@ -1,23 +1,27 @@
 ﻿using Aspire.Hosting;
 using Aspire.Hosting.ApplicationModel;
+using Aspire.Hosting.Eventing;
 using C3D.Extensions.Aspire.IISExpress.Annotations;
-using C3D.Extensions.Aspire.IISExpress.Configuration;
 using C3D.Extensions.Aspire.IISExpress.Resources;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
-using System.Xml.Serialization;
 
 namespace C3D.Extensions.Aspire.IISExpress;
 
 internal class IISEndPointConfigurator
 {
     private readonly DistributedApplicationModel appModel;
+    private readonly IDistributedApplicationEventing eventing;
     private readonly IOptions<IISExpressOptions> options;
     private readonly ILogger<IISEndPointConfigurator> logger;
 
-    public IISEndPointConfigurator(DistributedApplicationModel appModel, IOptions<IISExpressOptions> options, ILogger<IISEndPointConfigurator> logger)
+    public IISEndPointConfigurator(DistributedApplicationModel appModel,
+        IDistributedApplicationEventing eventing,
+        IOptions<IISExpressOptions> options, 
+        ILogger<IISEndPointConfigurator> logger)
     {
         this.appModel = appModel;
+        this.eventing = eventing;
         this.options = options;
         this.logger = logger;
     }
@@ -41,12 +45,43 @@ internal class IISEndPointConfigurator
     }
 
     private void AddBindings(IISExpressProjectResource project, SiteArgumentAnnotation site, ConfigArgumentAnnotation cfg)
-    {
-        var siteConfig = GetSiteConfig(cfg.ApplicationHostConfig, site.Site);
-
-        if (!project.HasAnnotationOfType<AppPoolArgumentAnnotation>())
+    { 
+        var appConfig = cfg.LoadConfiguration();
+        var existingConfig = appConfig is not null;
+        if (appConfig is null)
         {
-            project.Annotations.Add(new AppPoolArgumentAnnotation(siteConfig?.Application.ApplicationPool ?? AppPoolArgumentAnnotation.DefaultAppPool));
+            logger.LogWarning("Could not load application host config {AppHostConfig}", cfg.ApplicationHostConfig);
+            logger.LogInformation("Creating {AppHostConfig} from template", cfg.ApplicationHostConfig);
+            appConfig = project.Bitness.GetDefaultConfiguration();
+            SaveConfig();
+        }
+
+        void SaveConfig()
+        {
+            if (existingConfig)
+            {
+                cfg = project.WithTemporaryConfiguration(appConfig);
+                logger.LogDebug("Using temp configuration {AppHostConfig}", cfg.ApplicationHostConfig);
+                existingConfig = false;
+            }
+            else
+            {
+                cfg.SaveConfiguration(appConfig);
+            }
+        }
+
+        var siteConfig = appConfig.GetSite(site.Site);
+        if (!project.TryGetLastAnnotation<AppPoolArgumentAnnotation>(out var appPool))
+        {
+            appPool = new AppPoolArgumentAnnotation(siteConfig?.Application.ApplicationPool ?? AppPoolArgumentAnnotation.DefaultAppPool);
+            project.Annotations.Add(appPool);
+        }
+        if (!appConfig.SystemApplicationHost.ApplicationPools.Add
+            .Any(a => string.Equals(a.Name, appPool.AppPool, StringComparison.OrdinalIgnoreCase)))
+        {
+            logger.LogWarning("Application pool {AppPool} not found in {AppHostConfig}", appPool.AppPool, cfg.ApplicationHostConfig);
+            // <add name="Clr4IntegratedAppPool" managedRuntimeVersion="v4.0" managedPipelineMode="Integrated" CLRConfigFile="%IIS_USER_HOME%\config\aspnet.config" autoStart="true" />
+            appConfig.SystemApplicationHost.ApplicationPools.Add.Add(appPool.CreateIISConfigApplicationPool());
         }
 
         if (siteConfig is not null)
@@ -58,48 +93,44 @@ internal class IISEndPointConfigurator
                 if (!sitePath.Equals(projectPath))
                 {
                     logger.LogWarning("Site {Site} physical path {SitePath} does not match project path {ProjectPath}", site.Site, sitePath, projectPath);
+                    siteConfig.Application.VirtualDirectory.PhysicalPath = projectPath;
+                    SaveConfig();
                 }
             }
+            
+            var matches = new Dictionary<EndpointAnnotation, Configuration.Binding>();
             foreach (var binding in siteConfig.Bindings.Binding)
             {
-                AddBinding(project, binding);
+                var endpoint = project.AddOrUpdateEndpointFromBinding(binding);
+                matches[endpoint] = binding;
             }
+
+            var endpoints = project.Annotations.OfType<EndpointAnnotation>().Except(matches.Keys).ToList();
+            if (endpoints.Count != 0)
+            {
+                siteConfig.Bindings.Binding.AddRange(endpoints.Select(e => e.CreateIISConfigBinding()));
+                SaveConfig();
+            }
+
+            project.ShowIISExpressHttpsEndpointInformation(eventing, logger, project.Bitness);
         }
         else
         {
-            logger.LogError("Site {Site} not found in {AppHostConfig}", site.Site, cfg.ApplicationHostConfig);
+            logger.LogWarning("Site {Site} not found in {AppHostConfig}", site.Site, cfg.ApplicationHostConfig);
+
+            if (!project.TryGetEndpoints(out var endpoints) || !endpoints.Any())
+            {
+                logger.LogWarning("No endpoints found for project {Project}", project.Name);
+                project.WithDefaultIISExpressEndpoints();
+                endpoints = project.Annotations.OfType<EndpointAnnotation>();
+            }
+
+            var siteId = appConfig.SystemApplicationHost.Sites.Site.Max(s => int.TryParse(s.Id, out var id) ? id : 0) + 1;
+            var newSite = project.CreateIISConfigSite(siteId, site.Site, project.WorkingDirectory, appPool.AppPool, logger);
+            appConfig.SystemApplicationHost.Sites.Site.Add(newSite);
+            SaveConfig();
+
+            project.ShowIISExpressHttpsEndpointInformation(eventing, logger, project.Bitness);
         }
-    }
-
-    private static void AddBinding(IISExpressProjectResource project, Binding binding)
-    {
-        var endpoint = project.Annotations
-                            .OfType<EndpointAnnotation>()
-                            .Where(ea => StringComparer.OrdinalIgnoreCase.Equals(ea.Name, binding.Protocol))
-                            .SingleOrDefault();
-
-        if (endpoint is null)
-        {
-            endpoint = new EndpointAnnotation(System.Net.Sockets.ProtocolType.Tcp, name: binding.Protocol);
-            project.Annotations.Add(endpoint);
-        }
-
-        endpoint.Port = binding.Port;
-        endpoint.UriScheme = binding.Protocol;
-        endpoint.IsProxied = false;
-    }
-
-    private static Site? GetSiteConfig(string appHostConfigPath, string siteName)
-    {
-        var serializer = new XmlSerializer(typeof(ApplicationHostConfiguration));
-        using var reader = new FileStream(appHostConfigPath, FileMode.Open);
-
-        if (serializer.Deserialize(reader) is not ApplicationHostConfiguration appHostConfig)
-        {
-            return null;
-        }
-
-        return appHostConfig.SystemApplicationHost.Sites.Site
-            .SingleOrDefault(s => string.Equals(s.Name, siteName, StringComparison.OrdinalIgnoreCase));
     }
 }
