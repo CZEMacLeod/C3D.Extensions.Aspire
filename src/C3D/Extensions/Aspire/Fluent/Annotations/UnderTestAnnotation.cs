f296b98f54ce21ecef6f62d1d0159371297f3777
Cynthia Z E MacLeod (C3D) <cynthia.macleod@cloud3d.ltd>
2025-05-12 10:38:49 +0100

    Add Fluent project

---
+++ src/C3D/Extensions/Aspire/Fluent/Annotations/UnderTestAnnotation.cs
@@ -0,0 +1,12 @@
+﻿using Aspire.Hosting;
+using Aspire.Hosting.ApplicationModel;
+using System.Diagnostics;
+
+namespace C3D.Extensions.Aspire.Fluent.Annotations;
+
+public class UnderTestAnnotation : IResourceAnnotation
+{
+    public UnderTestAnnotation() => IsUnderTest = new StackTrace().ContainsAspireTesting();
+
+    public bool IsUnderTest { get; set; }
+}
---
+++ src/C3D/Extensions/Aspire/Fluent/UnderTestExtensions.cs
@@ -0,0 +1,37 @@
+﻿using Aspire.Hosting.ApplicationModel;
+using C3D.Extensions.Aspire.Fluent.Annotations;
+using Microsoft.Extensions.Hosting;
+using System.Diagnostics;
+
+namespace Aspire.Hosting;
+
+public static class UnderTestExtensions
+{
+    public static bool IsUnderTest<TResource>(this IResourceBuilder<TResource> resourceBuilder)
+        where TResource : IResource
+    {
+        if (!resourceBuilder.Resource.TryGetLastAnnotation<UnderTestAnnotation>(out var uta))
+        {
+            uta = new();
+            resourceBuilder.WithAnnotation(uta, ResourceAnnotationMutationBehavior.Replace);
+        }
+        return uta.IsUnderTest;
+    }
+
+    public static bool IsUnderTest(this IDistributedApplicationBuilder _) => new StackTrace().ContainsAspireTesting();
+    public static bool IsUnderTest(this IHostEnvironment _) => new StackTrace().ContainsAspireTesting();
+
+    public static IResourceBuilder<TResource> WhenUnderTest<TResource>(this IResourceBuilder<TResource> resourceBuilder,
+            Func<IResourceBuilder<TResource>, IResourceBuilder<TResource>> testAction,
+            Func<IResourceBuilder<TResource>, IResourceBuilder<TResource>>? notTestAction = null
+        )
+        where TResource : IResource => resourceBuilder.IsUnderTest() ?
+                testAction(resourceBuilder) :
+                notTestAction?.Invoke(resourceBuilder) ?? resourceBuilder;
+
+    public static bool ContainsAspireTesting(this StackTrace callStack) =>
+        callStack.GetStackFrames().Any(sf =>
+            sf.GetMethod()?.DeclaringType?.Assembly.GetName().Name == "Aspire.Hosting.Testing");
+
+    private static StackFrame[] GetStackFrames(this StackTrace callStack) => callStack.GetFrames() ?? [];
+}
