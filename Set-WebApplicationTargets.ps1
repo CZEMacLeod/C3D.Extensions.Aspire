1ab99051e205e8d8aa755c563aca426c1a9df4df
Cynthia Z E MacLeod (C3D) <cynthia.macleod@cloud3d.ltd>
2025-06-10 18:44:12 +0100

    Remove Set-WebApplicationTargets.ps1

--- Set-WebApplicationTargets.ps1
+++
@@ -1,4 +0,0 @@
-$vs_path = vswhere -latest -property installationPath
-$vs_tools_path = "$vs_path\MSBuild\Microsoft\VisualStudio\v17.0"
-$web_applications_target_path = "$vs_tools_path\WebApplications\Microsoft.WebApplication.targets"
-$env:WebApplicationsTargetPath=$web_applications_target_path
